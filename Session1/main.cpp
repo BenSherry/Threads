082b5e67d7999e9c955410eb6d3825d28c884fbc
杨 <YangChao61@outlook.com>
2020-06-03 07:42:53 +0000

    1.remove yield function 2. fix CMakeLists error

--- Session1/main.cpp
+++ Session1/main.cpp
@@ -4,60 +4,8 @@
 
 #include "Condition.hpp"
 
-int g_ExecuteSign = 0;
-void FuncPrintA()
-{
-    auto count = 10;
-    while(count > 0)
-    {
-        if (1 != g_ExecuteSign)
-        {
-            std::this_thread::yield();
-        }
-        std::cout << "This is from A \n";
-        count--;
-        g_ExecuteSign = 2;
-    }
-}
-
-void FuncPrintB()
-{
-    auto count = 10;
-    while(count > 0)
-    {
-        if (2 != g_ExecuteSign)
-        {
-            std::this_thread::yield();
-        }
-        std::cout << "This is from B \n";
-        count--;
-        g_ExecuteSign = 3;
-    }
-}
-
-void FuncPrintC()
-{
-    auto count = 10;
-    while(count > 0)
-    {
-        if (3 != g_ExecuteSign)
-        {
-            std::this_thread::yield();
-        }
-        std::cout << "This is from C \n";
-        count--;
-        g_ExecuteSign = 1;
-    }
-}
-
 int main()
 {
-    std::thread threadA {FuncPrintA};
-    std::thread threadB {FuncPrintB};
-    std::thread threadC {FuncPrintC};
-    threadA.join();
-    threadB.join();
-    threadC.join();
     PrintABC();
     std::cout <<"please add your code and have fun...\n";
 }
